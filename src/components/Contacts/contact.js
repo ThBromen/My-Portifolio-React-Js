0891d2743e9608e4537495f425cd5fde0bc610a2
ThBromen <hashimwimanatheogene34@gmail.com>
2024-07-12 13:28:15 +0200

    adding contact component

--- src/components/Contacts/contact.js
+++ src/components/Contacts/contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./contact.css"
+import "./contact.css";
 import client1 from '../../Assets/client1.jpg';
 import client2 from '../../Assets/client2.jpg';
 import client3 from '../../Assets/client3.jpg';
@@ -11,8 +11,8 @@ import youtube from '../../Assets/youtube icon.png';
 import Twiter from '../../Assets/twitter icon.png';
 import facebook from '../../Assets/facebook icon.png';
 import tickTock from '../../Assets/tik-tok icon.png';
-import microsoft from '../../Assets/microsoft icon.png'
-import email from '../../Assets/gmail icon.png'
+import microsoft from '../../Assets/microsoft icon.png';
+import email from '../../Assets/gmail icon.png';
 
 const Contact = () => {
   return (
@@ -28,32 +28,32 @@ const Contact = () => {
           <div className='clientComp'>
             <img src={client1} alt='client 1' className='clientImg' />
             <div className='clientDetails'>
-              <span className='clientName'>Emily Davis </span>
-              <span className='clientPosition'>CEO of Alex Johnson Enterprises </span>
+              <span className='clientName'>Emily Davis</span>
+              <span className='clientPosition'>CEO of Alex Johnson Enterprises</span>
               <button className='clientBtn'>Contact</button>
             </div>
           </div>
           <div className='clientComp'>
             <img src={client2} alt='client 2' className='clientImg' />
             <div className='clientDetails'>
-              <span className='clientName'>Michael Thompson </span>
-              <span className='clientPosition'>CTO of Sierra Tech Innovations </span>
+              <span className='clientName'>Michael Thompson</span>
+              <span className='clientPosition'>CTO of Sierra Tech Innovations</span>
               <button className='clientBtn'>Contact</button>
             </div>
           </div>
           <div className='clientComp'>
             <img src={client3} alt='client 3' className='clientImg' />
             <div className='clientDetails'>
-              <span className='clientName'>Sarah Martinez </span>
-              <span className='clientPosition'>Project Manager  </span>
+              <span className='clientName'>Sarah Martinez</span>
+              <span className='clientPosition'>Project Manager</span>
               <button className='clientBtn'>Contact</button>
             </div>
           </div>
           <div className='clientComp'>
             <img src={client4} alt='client 4' className='clientImg' />
             <div className='clientDetails'>
-              <span className='clientName'>David Lee  </span>
-              <span className='clientPosition'>Head of Development  </span>
+              <span className='clientName'>David Lee</span>
+              <span className='clientPosition'>Head of Development</span>
               <button className='clientBtn'>Contact</button>
             </div>
           </div>
@@ -61,27 +61,26 @@ const Contact = () => {
       </div>
 
       <div id='contact'>
-  <h1 className='contactPageTitle'>Contact Me</h1>
-  <span className='contactDesc'>Please fill out the form below to discuss any work opportunity</span>
-  <form className='contactForm'>
-    <input type='text' className='name' placeholder='Your name' />
-    <input type='email' className='email' placeholder='Your Email' />
-    <textarea rows={5} placeholder='Your Message' className='msg'></textarea>
-    <button type='submit' className='submitBtn'>Submit</button>
-    <div className='links'>
-      <img src={instagram} alt='Instagram' className='link' />
-      <img src={tickTock} alt='TikTok' className='link' />
-      <img src={watsup} alt='WhatsApp' className='link' />
-      <img src={linkedIn} alt='LinkedIn' className='link' />
-      <img src={youtube} alt='YouTube' className='link' />
-      <img src={Twiter} alt='Twitter' className='link' />
-      <img src={facebook} alt='Facebook' className='link' />
-      <img src={microsoft} alt='microsoft' className='link' />
-      <img src={email} alt='email' className='link' />
-    </div>
-  </form>
-</div>
-
+        <h1 className='contactPageTitle'>Contact Me</h1>
+        <span className='contactDesc'>Please fill out the form below to discuss any work opportunity</span>
+        <form className='contactForm'>
+          <input type='text' className='name' placeholder='Your name' />
+          <input type='email' className='email' placeholder='Your Email' />
+          <textarea rows={5} placeholder='Your Message' className='msg'></textarea>
+          <button type='submit' className='submitBtn'>Submit</button>
+          <div className='links'>
+            <img src={instagram} alt='Instagram' className='link' />
+            <img src={tickTock} alt='TikTok' className='link' />
+            <img src={watsup} alt='WhatsApp' className='link' />
+            <img src={linkedIn} alt='LinkedIn' className='link' />
+            <img src={youtube} alt='YouTube' className='link' />
+            <img src={Twiter} alt='Twitter' className='link' />
+            <img src={facebook} alt='Facebook' className='link' />
+            <img src={microsoft} alt='Microsoft' className='link' />
+            <img src={email} alt='Email' className='link' />
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
